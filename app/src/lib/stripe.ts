24fb9129c5df5190754f58cde8eda003362e0277
PiloTracer <acsking2022@gmail.com>
2026-03-23 12:56:38 -0600

    fix: update stripe apiVersion to 2025-02-24.acacia

--- app/src/lib/stripe.ts
+++ app/src/lib/stripe.ts
@@ -10,5 +10,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = process.env.STRIPE_SECRET_KEY
-  ? new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2024-12-18.acacia' })
+  ? new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2025-02-24.acacia' })
   : null;
